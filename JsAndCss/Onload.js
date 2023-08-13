8c4adf6d49f08092921e61a6a9d3b12fcb4d3273
Bandysol <Bandysol@DESKTOP-H8E0PO4>
2023-08-13 21:22:46 +0800

    Release 1.2.0 RC3

--- JsAndCss/Onload.js
+++ JsAndCss/Onload.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 	var isp = IsPhone();
 	if (isp) {
-		window.location.href=window.location.host+"phone/"+window.location.pathname;
+		window.location.href="phone/"+window.location.pathname;
 	}
 	setTimeOut(startBjMusic);
 	setInterval(startBjMusic,10000);
