80b57cc98cfcd9dd2d836eddcff094ec15991c8b
Bandysol <Bandysol@DESKTOP-H8E0PO4>
2023-08-15 07:13:59 +0800

    Release 1.3.0 RC8.5

--- PublicPage/footercor_blob.js
+++ PublicPage/footercor_blob.js
@@ -1,8 +1,8 @@
 document.writeln("<div align=\"center\">");
 document.writeln("<li class=\"button-group\">");
 document.writeln("	<button class=\"large blue button\" id=\"back\"> 上一篇 </button>");
-document.writeln("	<button class=\"large blue button\" onclick=\"location.href='./';\"> 返回主页 </button>");
-document.writeln("	<button class=\"large blue button\" onclick=\"location.href='./list1.html';\"> 返回列表 </button>");
+document.writeln("	<button class=\"large blue button\" onclick=\"location.href='/';\"> 返回主页 </button>");
+document.writeln("	<button class=\"large blue button\" onclick=\"location.href='/list1.html';\"> 返回列表 </button>");
 document.writeln("	<button class=\"large blue button\" id=\"next\"> 下一篇 </button>");
 document.writeln("</li>");
 document.writeln("</div>");
