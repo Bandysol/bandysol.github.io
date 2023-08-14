69960479ccb3a9cf7afe5e3bdd430e2075fb7f03
Bandysol <Bandysol@DESKTOP-H8E0PO4>
2023-08-14 13:43:39 +0800

    Release 1.3.0 RC3.2

--- JsAndCss/Onload.js
+++ JsAndCss/Onload.js
@@ -3,6 +3,6 @@ window.onload = function() {
 	if (isp) {
 		window.location.href="phone"+window.location.pathname;
 	}
-	setTimeOut(startBjMusic);
+	setTimeout(startBjMusic);
 	setInterval(startBjMusic,10000);
 }
--- blog/NextAndBack.js
+++ blog/NextAndBack.js
@@ -3,7 +3,7 @@ window.onload = function() {
 	if (isp) {
 		window.location.href="phone"+window.location.pathname;
 	}
-	setTimeOut(startBjMusic);
+	setTimeout(startBjMusic);
 	setInterval(startBjMusic,10000);
 	CheckNextAndBack(window.location.pathname.replace("/blog/",""));
 }
