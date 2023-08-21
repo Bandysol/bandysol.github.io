5d57580f3c3d2bf02703f1d8e2641578a6303c3e
Bandysol <Bandysol@DESKTOP-H8E0PO4>
2023-08-21 14:10:08 +0800

    V1.3.0 RC10.2

--- PublicPage/listhead.js
+++ PublicPage/listhead.js
@@ -1,10 +1,10 @@
 document.writeln("<div align=\"center\">");
 document.writeln("<li class=\"button-group\">");
-document.writeln("	<button class=\"large blue button\" id=\"button-select-1\" onclick=\"location.href=\'./\';\"> 主页 </button>");
-document.writeln("	<button class=\"large blue button\" id=\"button-select-2\" onclick=\"location.href=\'./list1.html\';\"> 文章列表 </button>");
-document.writeln("	<button class=\"large blue button\" id=\"button-select-3\" onclick=\"location.href=\'./list2.html\';\"> 音频列表 </button>");
-document.writeln("	<button class=\"large blue button\" id=\"button-select-4\" onclick=\"location.href=\'./list3.html\';\"> 项目列表 </button>");
-document.writeln("	<button class=\"large blue button\" id=\"button-select-5\" onclick=\"location.href=\'./about.html\';\"> 关于本站 </button>");
+document.writeln("	<button class=\"big blue button\" id=\"button-select-1\" onclick=\"location.href=\'./\';\"> 主  页 </button>");
+document.writeln("	<button class=\"big blue button\" id=\"button-select-2\" onclick=\"location.href=\'./list1.html\';\"> 文章列表 </button>");
+document.writeln("	<button class=\"big blue button\" id=\"button-select-3\" onclick=\"location.href=\'./list2.html\';\"> 音频列表 </button>");
+document.writeln("	<button class=\"big blue button\" id=\"button-select-4\" onclick=\"location.href=\'./list3.html\';\"> 项目列表 </button>");
+document.writeln("	<button class=\"big blue button\" id=\"button-select-5\" onclick=\"location.href=\'./about.html\';\"> 关于本站 </button>");
 document.writeln("</li>");
 document.writeln("</div>");
 
